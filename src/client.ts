2ae7024c140f7fcf3d0a5ebf9c579cfabf86b0ea
Arianrhod Sandlot <theguidanceofawhitetower@gmail.com>
2025-11-24 19:29:18 +0800

    feat: update types for the client

--- src/client.ts
+++ src/client.ts
@@ -1,18 +1,23 @@
-import { hc, parseResponse } from 'hono/client'
+import { type ClientRequestOptions, hc, parseResponse } from 'hono/client'
 import type { apis } from './apis.ts'
 
-export function createClient(host = 'https://msleuth.arianrhodsandlot.workers.dev') {
-  const client = hc<typeof apis>(new URL('api/v1', host).href)
+type HC = ReturnType<typeof hc<typeof apis>>
+type Query = HC['metadata']['query']['$post']
+type Identify = HC['metadata']['identify']['$post']
+
+export type IdentifyParameter = Parameters<Identify>[0]['json']
+export type QueryParameter = Parameters<Query>[0]['json']
+
+export function createClient(host = 'https://msleuth.arianrhodsandlot.workers.dev', options?: ClientRequestOptions) {
+  const client = hc<typeof apis>(new URL('api/v1', host).href, options)
 
   return {
-    async identify(json: Parameters<typeof client.metadata.identify.$post>[0]['json']) {
-      const response = await parseResponse(client.metadata.identify.$post({ json }))
-      return response
+    async identify(json: IdentifyParameter) {
+      return await parseResponse(client.metadata.identify.$post({ json }))
     },
 
-    async query(json: Parameters<typeof client.metadata.query.$post>[0]['json']) {
-      const response = await parseResponse(client.metadata.query.$post({ json }))
-      return response
+    async query(json: QueryParameter) {
+      return await parseResponse(client.metadata.query.$post({ json }))
     },
   }
 }
